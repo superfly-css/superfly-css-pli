cb83d868469b7c1d41976267393fc073fbff9626
Ole Ersoy <ole.ersoy@gmail.com>
2015-11-05 17:19:59 -0600

    Added constants containing glob patterns for src files

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-module.exports = PLI = {
+var path = require('path');
+var GLOB = require('glob-patterns');
+
+var PLI = {
   SOURCE: './src/',
   TARGET: './target/',
   src: {
     main: {
-      css: this.SOURCE + 'main/css',
+      js: './src/main/js/',
+      css: './src/main/css/',
+      html: './src/main/html/'
     },
     test: {
-      js: this.SOURCE + 'test/js',
-      css: this.SOURCE + 'test/css',
-      html: this.SOURCE + 'test/html'
+      js: './src/test/js/',
+      css: './src/test/css/',
+      html: './src/test/html/'
     }
   },
   target: {
     main: {
-      js: this.TARGET + 'main/js',
-      css: this.TARGET + 'main/css',
-      html: this.TARGET + 'main/html'
+      js: './target/main/js/',
+      css: './target/main/css/',
+      html: './target/main/html/'
     },
     test: {
-      js: this.TARGET + 'test/js',
-      css: this.TARGET + 'test/css',
-      html: this.TARGET + 'test/html'
+      js: './target/test/js/',
+      css: './target/test/css/',
+      html: './target/test/html/'
     }
   }
 }
+
+var glob = path.join(PLI.src.test.html, GLOB.ALL_HTML);
+
+PLI.SRC_MAIN_CSS = path.join(PLI.src.main.css, GLOB.ALL_CSS);
+PLI.SRC_MAIN_HTML = path.join(PLI.src.main.html, GLOB.ALL_HTML);
+PLI.SRC_MAIN_JS = path.join(PLI.src.main.html, GLOB.ALL_JS);
+PLI.SRC_TEST_CSS = path.join(PLI.src.test.css, GLOB.ALL_CSS);
+PLI.SRC_TEST_HTML = path.join(PLI.src.test.html, GLOB.ALL_HTML);
+PLI.SRC_TEST_JS = path.join(PLI.src.test.html, GLOB.ALL_JS);
+
+module.exports = PLI;
